eb12865275f7e6969cacbc34a016c23555c927c2
Dhaval <kdkothari7@gmail.com>
2023-08-13 16:40:48 +0530

    error on luanch : dotenv pakage commented

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const dotenv = require('dotenv').config({path: path.resolve(__dirname, '../.env')})
+// const dotenv = require('dotenv').config({path: path.resolve(__dirname, '../.env')})
 
 const express = require('express')
 const fs = require('fs')
